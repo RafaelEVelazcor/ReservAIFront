3f5b066ae2ceb5ba0fb805139ea5f5155e12c967
Luis Guillermo Rivera <memoriverastephens@gmail.com>
2026-04-08 22:17:46 -0600

    Revert "ya funciona para fly.io"
    This reverts commit 544d7d1f5123f072a6f2eec6e3ea2befe14b1a1d.
    
    regresando a la funcion de fly.io

--- public/Scripts/router.js
+++ public/Scripts/router.js
@@ -15,7 +15,7 @@ router.use((req, res, next) => {
 // Definir rutas
 router.get('/', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/login.html")));
 router.get('/login', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/login.html")));
-router.get('/inicio', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/Inicio.html")));
+router.get('/inicio', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/inicio.html")));
 router.get('/register', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/register.html")));
 router.get('/QR', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/QR.html")));
 router.get('/verify_email', (req, res) => res.sendFile(path.resolve(__dirname + "/../views/verify_email.html")));
